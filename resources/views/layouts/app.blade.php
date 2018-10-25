a3b6ea46c9227411a996ac7daa5e235feb79fe7a
kurata <2ndwavekurata@gmail.com>
2018-10-25 06:47:01 +0000

    1024

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>Blog - @yield('title')</title>
+  </head>
+  <body>
+    <div class="container">
+      @yield('content')
+    </div>
+    <footer class="footer">
+  @yield('footer')
+</footer>
+  </body>
+</html>
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('title','記事一覧')
+
+@section('content')
+<table>
+<tr>
+  <th>title</th>
+  <th>content</th>
+  <th>created_at</th>
+</tr>
+@foreach($posts as $post)
+<tr>
+<td><li>{{ link_to_route('posts.show',$post->title,[$post->id]) }}</li></td>
+ <td>{{ $post->content }}</td>
+ <td>{{ $post->created_at }}</td>
+</tr>
+@endforeach
+</table>
+@endsection
+@section('footer')
+{{ link_to_route('posts.create','[記事作成]') }}
+@endsection
